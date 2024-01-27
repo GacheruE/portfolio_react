6b2a7eb36a34015284e56bce3ca878cbb602a2ac
GacheruE <nyangendogacheru@gmail.com>
2024-01-27 19:37:02 +0300

    refractor:move image from about to home

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import Photo from '../assets/Photo.jpg';
 
 const About = () => {
   return (
-    <div name='about' className='bg-[#39465a] py-12'>
+    <div name='about' className='bg-[#c8cfd9] py-12'>
       <div className='container mx-auto px-4'>
         <div className='flex flex-col items-center'>
-          <p className='text-4xl font-bold inline text-gray-200 mb-8'>About Me</p>
-          <div className='flex flex-col sm:flex-row items-center max-w-5xl mx-auto'>
-            <div className='sm:w-3/5 sm:pr-8 mb-4 sm:mb-0'>
-              <p className='text-left text-gray-300'>
-                I earned my Bachelor's degree in Computer Science from Strathmore University, where I delved into a range of computer science topics, from algorithms and data structures to software engineering principles. This academic journey has equipped me with a robust understanding of the core concepts that underpin effective software development. I find joy in turning creative ideas into functional and user-friendly websites. Whether it's building interactive interfaces, optimizing performance, or solving complex challenges, I'm driven by the desire to create seamless online experiences. The fast-paced nature of the tech industry motivates me to stay on the cutting edge. I am committed to continuous learning, keeping up with the latest web development trends, and exploring new technologies that enhance my skill set. While I thrive in the technical realm, I understand the importance of effective communication and collaboration. I approach challenges with a problem-solving mindset, always seeking to contribute positively to team dynamics. I am excited about the endless possibilities that web development offers, and I am ready to embark on a journey where I can apply my skills, learn, and contribute to building innovative solutions that make a positive impact. Let's connect and explore the boundless opportunities that lie ahead
-              </p>
-            </div>
-            <div className='w-full sm:w-2/5 h-80 sm:h-96 rounded-full overflow-hidden shadow-lg'>
-              <img src={Photo} alt='About' className='w-full h-full object-cover' />
-            </div>
+          <p className='text-4xl font-bold text-gray-900 mb-8 text-center'>About Me</p>
+          <div className='max-w-5xl mx-auto'>
+            <p className='text-justify text-gray-900'>
+              I earned my Bachelor's degree in Computer Science from Strathmore University, where I delved into a range of computer science topics, from algorithms and data structures to software engineering principles. This academic journey has equipped me with a robust understanding of the core concepts that underpin effective software development. I find joy in turning creative ideas into functional and user-friendly websites. Whether it's building interactive interfaces, optimizing performance, or solving complex challenges, I'm driven by the desire to create seamless online experiences. The fast-paced nature of the tech industry motivates me to stay on the cutting edge. I am committed to continuous learning, keeping up with the latest web development trends, and exploring new technologies that enhance my skill set. While I thrive in the technical realm, I understand the importance of effective communication and collaboration. I approach challenges with a problem-solving mindset, always seeking to contribute positively to team dynamics. I am excited about the endless possibilities that web development offers, and I am ready to embark on a journey where I can apply my skills, learn, and contribute to building innovative solutions that make a positive impact. Let's connect and explore the boundless opportunities that lie ahead
+            </p>
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import{HiArrowNarrowRight} from 'react-icons/hi'
-import { Link } from 'react-scroll'
+import React from 'react';
+import Photo from '../assets/Photo.jpg';
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#1f2630]'>
+    <div name='home' className='w-full h-screen bg-[#1f2630] flex items-center justify-center'>
 
-        {/*Container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-cyan-200 text-2xl sm:text-4xl'>Hi, my name is</p>
-            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Esther Gacheru</h1>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>A passionate computer science graduate with a keen interest in the world of web development. As a recent graduate, I bring fresh
-              perspectives, a solid foundation in computer science, and a strong commitment to crafting innovative solutions</p>
-        <div>
-            <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-cyan-200 hover:border-cyan-200'>View Work
-            <HiArrowNarrowRight className='ml-3' /> </button>
-        </div>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-6 flex flex-col sm:flex-row justify-between h-full'>
+
         
+        <div className='sm:w-1/2 sm:pr-2 flex flex-col justify-center'>
+          <p className='text-cyan-200 text-2xl sm:text-4xl'>Hi, my name is</p>
+          <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Esther Gacheru</h1>
+          <p className='text-[#8892b0] py-4 max-w-[900px] text-xl'>A passionate computer science graduate with a keen interest in the world of web development. As a recent graduate, I bring fresh perspectives, a solid foundation in computer science, and a strong commitment to crafting innovative solutions</p>
         </div>
+
+        <div className='sm:w-1/2 sm:pl-5 flex justify-center items-center'>
+          <img src={Photo} alt='Esther Gacheru' className='w-64 h-64 sm:w-80 sm:h-80 rounded-full shadow-lg' />
+        </div>
+
+      </div>
+
     </div>
   )
 }
 
-export default Home
+export default Home;
