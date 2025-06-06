2c89e429bfb1dcf6715cd7c48608903eba252aa6
GacheruE <109478273+GacheruE@users.noreply.github.com>
2025-06-07 01:25:07 +1000

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,9 @@ const About = () => {
           <p className='text-4xl font-bold text-gray-900 mb-8 text-center'>About Me</p>
           <div className='max-w-5xl mx-auto'>
             <p className='text-justify text-gray-900'>
-            Through internships in the finance and agriculture sectors, I’ve gained practical experience in predictive modelling, automation, and dashboarding using tools like Python, SQL, Power BI, and Excel. I've built fraud detection models, OCR systems, and data-driven reports that support real-world decision-making. I thrive in collaborative environments, value continuous learning, and am driven by the opportunity to create solutions that are both technically sound and impactful.
+            I’m a Computer Science and Informatics graduate currently pursuing a Master of Data Science (Professional), with a strong passion for transforming data into meaningful insights. My academic background and project experience span both software development and data analytics, giving me a well-rounded technical foundation.
+
+Through internships in the finance and agriculture sectors, I’ve gained practical experience in predictive modelling, automation, and dashboarding using tools like Python, SQL, Power BI, and Excel. I've built fraud detection models, OCR systems, and data-driven reports that support real-world decision-making. I thrive in collaborative environments, value continuous learning, and am driven by the opportunity to create solutions that are both technically sound and impactful.
             </p>
           </div>
         </div>
